a8952b5eb79b31e8a4f68da95b697e9ea33bf034
AmrSaeed3 <amrsaid4343@gmail.com>
2023-12-24 00:40:31 +0200

    decrese expireIn token

--- utils/generate.jwt.js
+++ utils/generate.jwt.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 secretKey = process.env.JWT_SECLET_KEY;
 const generate = async (payload) => {
-  const expireIn = "5m";
+  const expireIn = "3m";
   const token = await jwt.sign(payload, secretKey, { expiresIn: expireIn });
   const expireData = jwt.decode(token).exp * 1000 - Date.now();
   return { expireData, token: token, expireIn };
