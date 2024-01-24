bc93511f768f743f9f324f91154d4e3224b13781
AmrSaeed3 <amrsaid4343@gmail.com>
2024-01-24 22:42:59 +0200

    add data as pdf

--- Controllers/first aid.controllers.js
+++ Controllers/first aid.controllers.js
@@ -1,5 +1,6 @@
 const { user1 } = require("../Models/firs aid.models");
 const mammoth = require("mammoth");
+const pdf = require("pdf-parse");
 const fs = require("fs");
 const path = require("path");
 const appError = require("../utils/appError");
@@ -54,7 +55,9 @@ const addChapter = async (req, res, next) => {
             // استخراج الحروف فقط
             const lettersOnly = sentence.replace(/[^a-zA-Z\s]/g, "");
             //إزالة المسافة في بداية الجملة
-            const cleanedSentence = lettersOnly.replace(/^\s+/, "").replace(/\s+$/, '');
+            const cleanedSentence = lettersOnly
+              .replace(/^\s+/, "")
+              .replace(/\s+$/, "");
             return cleanedSentence;
           }
 
@@ -116,6 +119,7 @@ const addChapter = async (req, res, next) => {
       });
   });
 };
+
 //modify size page
 // const chapter1 = (req, res) => {
 //   // const pageSize = 200; // حجم الصفحة
@@ -194,9 +198,123 @@ const chapter = async (req, res, next) => {
     data: chapter.paragraphs[numbers - 1],
   });
 };
+const addChapterpdf = async (req, res, next) => {
+  if (!req.file) {
+    const error = appError.create(
+      "The file was not uploaded",
+      400,
+      statusText.FAIL
+    );
+    return next(error);
+  }
+  const paragraphMarker = "#"; // يمكنك تغيير هذا إلى الرمز الذي قمت بوضعه في ملف Word
+  const filePath = req.file.filename;
+  const name = filePath.split(".")[0];
+  const extension = filePath.split(".")[1];
+  if (!filePath) {
+    const error = appError.create(
+      "File path must be provided",
+      400,
+      statusText.ERROR
+    );
+    return next(error);
+  }
+
+  const absolutePath = path.resolve(__dirname, "..", "file", filePath);
+  // قراءة محتوى الملف PDF
+  fs.readFile(absolutePath, (err, data) => {
+    if (err) {
+      const error = appError.create(
+        "An error occurred while reading the file",
+        500,
+        statusText.ERROR
+      );
+      return next(error);
+    }
+    // استخدام مكتبة pdf-parse لتحليل الملف
+    pdf(data)
+      .then(async (result) => {
+        // data.text يحتوي على نص الملف
+        const paragraphs = result.text.split(paragraphMarker);
+        // إضافة ترقيم لكل فقرة
+        const numberedParagraphs = paragraphs.map((paragraph, index) => {
+          const paragraphNumber = index + 1;
+          const lines = paragraph.split("\n");
+          const firstLine = lines.length > 0 ? lines[0].trim() : "";
+
+          function extractLetters(sentence) {
+            // استخراج الحروف فقط
+            const lettersOnly = sentence.replace(/[^a-zA-Z\s]/g, "");
+            //إزالة المسافة في بداية الجملة
+            const cleanedSentence = lettersOnly
+              .replace(/^\s+/, "")
+              .replace(/\s+$/, "");
+            return cleanedSentence;
+          }
+
+          const result = extractLetters(firstLine);
+          const currentPhoto = `${req.protocol}://${req.get(
+            "host"
+          )}/uploads/${name}/${result}.png`;
+          return {
+            pageNumber: paragraphNumber,
+            title: firstLine,
+            text: paragraph.replace(firstLine, "").trim(),
+            currentPhoto: currentPhoto,
+          };
+        });
+        const chapter = await chapterModel.findOne({ name: name });
+        if (chapter) {
+          const fileName = req.file.filename; // اسم الملف الذي تريد حذفه
+          const filePathToDelete = path.join(__dirname, "..", "file", fileName); // تحديد الملف بناءً على المجلد الجذر
+          fs.unlink(filePathToDelete, (err) => {
+            if (err) {
+              const error = appError.create(
+                "wrong in the delete data",
+                400,
+                httpStatus.FAIL
+              );
+              return next(error);
+            }
+          });
+          const error = appError.create(
+            "this chapter is already exist",
+            400,
+            statusText.FAIL
+          );
+          return next(error);
+        }
+        const newaway = new chapterModel({
+          name: name,
+          extension: extension,
+          totalParagraphs: paragraphs.length,
+          paragraphs: numberedParagraphs,
+        });
+        await newaway.save();
+
+        // ارسل النص المرقم والفقرة المحددة
+        res.json({
+          message: "The file has been uploaded successfully.",
+          name: name,
+          totalParagraphs: paragraphs.length,
+          paragraphs: numberedParagraphs,
+        });
+      })
+      .catch((err) => {
+        const error = appError.create(
+          "An error occurred while processing the file2",
+          500,
+          statusText.ERROR
+        );
+        return next(error);
+      });
+  });
+};
+
 module.exports = {
   addChapter,
   allChapter,
   //   chapter1,
+  addChapterpdf,
   chapter,
 };
--- Routes/first aid.routes.js
+++ Routes/first aid.routes.js
@@ -6,6 +6,9 @@ const firstAidController = require("../Controllers/first aid.controllers");
 router
   .route("/addChapter")
   .post(uploadWord.single("chapter"), firstAidController.addChapter);
+router
+  .route("/addChapterpdf")
+  .post(uploadWord.single("chapter"), firstAidController.addChapterpdf);
 router.route("/readAll/:name").get(firstAidController.allChapter);
 router.route("/read-Chapter/:name/:num").get(firstAidController.chapter);
 module.exports = router;
